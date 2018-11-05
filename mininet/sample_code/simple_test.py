e214b31ed12fdb5fbf0b4261958200d561fe9342
Tri Nguyen <minhtribk@gmail.com>
2018-11-05 09:18:50 +0700

    add Remote controller

--- mininet/sample_code/simple_test.py
+++ mininet/sample_code/simple_test.py
@@ -4,6 +4,7 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.node import RemoteController
 
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
